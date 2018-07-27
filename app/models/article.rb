c280e78908063302e9656bde04b797b956d47dcf
Sujnesh Mishra <sujneshmishra@Axe.local>
2018-07-27 16:47:09 +0530

    fixed bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,7 +15,17 @@ class Article < ActiveRecord::Base
 
 	def tag_list=(tags_string)
 		tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
-		new_or_found_tags = tag_names.collect{ |name| Tag.find_or_create_by(name: name)}
-		self.tags << new_or_found_tags
+		new_or_found_tags = tag_names.collect{ |name| Tag.find_or_create_by(name: name)}.uniq
+		otags = self.tags
+		var = 1
+		otags.collect{ |old|
+			if old.name == new_or_found_tags.first.name
+				var = 0
+			end
+		}
+		if var == 1 
+			self.tags << new_or_found_tags
+		end
+		self.tags
 	end
 end
